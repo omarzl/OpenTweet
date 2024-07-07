08ebf980712b78f763482eff73b54b3f10244350
omarzl <omarzl@hotmail.es>
2024-07-07 17:37:01 -0600

    Added thread feature modules

---
+++ TweetThreadFeature/TweetThreadFeature.swift
@@ -0,0 +1,10 @@
+//
+//  TweetThreadFeature.swift
+//  TweetThreadFeature
+//
+//  Created by Omar Zúñiga Lagunas on 07/07/24.
+//
+
+class TweetThreadFeature {
+
+}
---
+++ TweetThreadFeatureInterface/TweetThreadFeatureInterface.swift
@@ -0,0 +1,10 @@
+//
+//  TweetThreadFeatureInterface.swift
+//  TweetThreadFeatureInterface
+//
+//  Created by Omar Zúñiga Lagunas on 07/07/24.
+//
+
+class TweetThreadFeatureInterface {
+
+}
