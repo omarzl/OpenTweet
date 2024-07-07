a5c32abf9504c75ade0f0ec146ef0cc1ad5f72a8
omarzl <omarzl@hotmail.es>
2024-07-07 14:11:33 -0600

    deleted swift data support from timeline since we dont need to track it

--- TweetFoundation/Timeline.swift
+++ TweetFoundation/Timeline.swift
@@ -9,17 +9,11 @@ import Foundation
 import SwiftData
 
 /// Representation a timeline of tweets
-@Model
 final public class Timeline: Decodable {
     
-    let tweets: [Tweet]
+    public let tweets: [Tweet]
     
     enum CodingKeys: String, CodingKey {
         case tweets = "timeline"
-    }
-    
-    required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        tweets = try container.decode([Tweet].self, forKey: .tweets)
     }
 }
