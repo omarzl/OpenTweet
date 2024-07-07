c0f6db2b74d94808788a4283ea4344d123186d2b
omarzl <omarzl@hotmail.es>
2024-07-07 13:04:48 -0600

    create static lib TweetFoundation which will share models between features

---
+++ TweetFoundation/Tweet.swift
@@ -0,0 +1,55 @@
+//
+//  TweetFoundation.swift
+//  TweetFoundation
+//
+//  Created by Omar Zúñiga Lagunas on 07/07/24.
+//
+
+import Foundation
+import SwiftData
+
+/// Representation of a tweet
+@Model
+final public class Tweet: Decodable {
+    /// Unique identifier
+    public let id: String
+    /// Author handle
+    public let author: String
+    /// Content of the tweet
+    public let content: String
+    /// Avatar image of the author
+    public let avatar: String?
+    /// Date when the tweet was published, in the format ISO-8601
+    public let date: Date
+    /// Replies from other users to this tweet
+    public var replies = [Tweet]()
+    
+    enum CodingKeys: CodingKey {
+        case id, author, content, avatar, date
+    }
+    
+    public init(
+        id: String,
+        author: String,
+        content: String,
+        avatar: String?,
+        date: Date,
+        replies: [Tweet]
+    ) {
+        self.id = id
+        self.author = author
+        self.content = content
+        self.avatar = avatar
+        self.date = date
+        self.replies = replies
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        author = try container.decode(String.self, forKey: .author)
+        content = try container.decode(String.self, forKey: .content)
+        avatar = try container.decode(String.self, forKey: .avatar)
+        date = try container.decode(Date.self, forKey: .date)
+    }
+}
