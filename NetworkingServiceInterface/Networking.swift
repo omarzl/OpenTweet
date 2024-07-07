a8fee3bef2c1c5675754df0320c2aefab9e0db0c
omarzl <omarzl@hotmail.es>
2024-07-06 19:01:38 -0600

    added docs

--- NetworkingServiceInterface/Networking.swift
+++ NetworkingServiceInterface/Networking.swift
@@ -5,6 +5,10 @@
 //  Created by Omar Zúñiga Lagunas on 06/07/24.
 //
 
+/// Abstraction of RESTful network requests
 public protocol Networking {
+    /// Calls an url with a GET request
+    /// - Parameter url: URL to be called
+    /// - Returns: Returns a parsed model of type `Decodable`
     func get<Model: Decodable>(from url: String) async throws -> Model
 }
