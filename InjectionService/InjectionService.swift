21995c09e14bc3554912582239fb3a29c3796d1c
omarzl <omarzl@hotmail.es>
2024-07-06 18:13:42 -0600

    created InjectionService dynamic framework

---
+++ InjectionService/InjectionService.swift
@@ -0,0 +1,8 @@
+//
+//  InjectionService.swift
+//  InjectionService
+//
+//  Created by Omar Zúñiga Lagunas on 06/07/24.
+//
+
+import Foundation
---
+++ InjectionServiceTests/InjectionServiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  InjectionServiceTests.swift
+//  InjectionServiceTests
+//
+//  Created by Omar Zúñiga Lagunas on 06/07/24.
+//
+
+import XCTest
+@testable import InjectionService
+
+final class InjectionServiceTests: XCTestCase {
+
+}
